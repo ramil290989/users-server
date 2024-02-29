b39446f154bcef24b2fe8b2444e34a20979509c4
ramil290989 <garifullinramil290989@gmail.com>
2024-02-29 20:17:47 +0500

    add Headers

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const { PORT } = process.env;
 
 const start = () => {
   const app = express();
+  app.use(express.static('images'));
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use('/api', router);
